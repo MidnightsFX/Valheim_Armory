f5731cad7d2d8d577352c6a41416f331af753c72
MidnightsFX <MidnightsFX@users.noreply.github.com>
2021-08-23 21:40:55 -0700

    Initial commit

---
+++ JotunnModStub/JotunnModStub.cs
@@ -0,0 +1,45 @@
+﻿// JotunnModStub
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    JotunnModStub.cs
+// Project: JotunnModStub
+
+using BepInEx;
+
+namespace JotunnModStub
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class JotunnModStub : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.jotunn.jotunnmodstub";
+        public const string PluginName = "JotunnModStub";
+        public const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+            // Jotunn comes with MonoMod Detours enabled for hooking Valheim's code
+            // https://github.com/MonoMod/MonoMod
+            On.FejdStartup.Awake += FejdStartup_Awake;
+            
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("ModStub has landed");
+            
+            // To learn more about Jotunn's features, go to
+            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+        }
+
+        private void FejdStartup_Awake(On.FejdStartup.orig_Awake orig, FejdStartup self)
+        {
+            // This code runs before Valheim's FejdStartup.Awake
+            Jotunn.Logger.LogInfo("FejdStartup is going to awake");
+
+            // Call this method so the original game method is invoked
+            orig(self);
+
+            // This code runs after Valheim's FejdStartup.Awake
+            Jotunn.Logger.LogInfo("FejdStartup has awoken");
+        }
+    }
+}
