34667731b2d294317125ba67652022ad74855a90
Carl Stutz <carl.stutz@gmail.com>
2024-04-15 22:24:50 -0700

    update translations for affected weapon descriptions

---
+++ reset_select_language_entries.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "json"
+
+language_files = Dir["ValheimArmory/localizations/*"]
+keys_to_remove = %w[item_arrow_chitin_description item_bolt_silver_frost_description item_silver_atgeir_description item_iron_greatsword_description]
+
+language_files.each do |lang_file|
+  next if lang_file == "ValheimArmory/localizations/English.json"
+
+  lang_json = JSON.parse(File.read("#{lang_file}"))
+  keys_to_remove.each do |rm_key|
+    lang_json.delete(rm_key)
+  end
+  File.open("#{lang_file}", "w") { |f| f.write(JSON.pretty_generate(lang_json)) }
+end
